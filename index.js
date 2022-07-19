ae8015781b9fa48c88ee32fd4c9cd6e787ac1289
Matheus Pereira <matheusdimmy@hotmail.com>
2022-07-19 01:09:01 -0300

    feat: Criado endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -22,4 +22,22 @@ app.get('/talker', async (_req, res, next) => {
   }
 });
 
+app.get('/talker/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const talkers = JSON.parse(await fs.readFile('talker.json'));
+    const talkerById = talkers.find((r) => r.id === Number(id));
+
+    if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    return res.status(200).json(talkerById);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: err.message });
+});
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
